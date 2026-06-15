55ed9a99f6f5a6af7b5f137a1a05272cd747eeab
Guruprasath Manika Rameshbabu <44829747+gru13@users.noreply.github.com>
2026-06-15 13:11:13 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+        number_of_island = 0
+        rows,cols = len(grid), len(grid[0])
+        def check_out_surrounding(i,j):
+            nonlocal rows, cols
+            if i >= rows or i < 0:
+                return 
+            if j >= cols or j < 0:
+                return
+            if grid[i][j] == '0' or (i,j) in visited:
+                return
+            visited.add((i,j))
+            check_out_surrounding(i-1,j) #top       
+            check_out_surrounding(i+1,j) #dwn       
+            check_out_surrounding(i,j-1) #lft       
+            check_out_surrounding(i,j+1) #ryt            
+
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1' and (i,j) not in visited:
+                    check_out_surrounding(i,j)
+                    number_of_island += 1
+
+
+        return number_of_island
+                    
