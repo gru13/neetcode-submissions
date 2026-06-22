54fa625d0b453b491b72a15933c0269d42a8a36e
Guruprasath Manika Rameshbabu <44829747+gru13@users.noreply.github.com>
2026-06-22 22:17:23 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,15 @@
+from collections import Counter, defaultdict
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tc = Counter(t)
+        window = Counter()
+        left = 0
+        word = ""        
+        for right in range(len(s)):
+            window[s[right]] += 1
+            while window >= tc:
+                cur_word = s[left:right+1]
+                word = cur_word if len(word) > len(cur_word) or word == "" else word    
+                window[s[left]] -= 1
+                left += 1
+        return word
